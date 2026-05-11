f905631e7e16fdc6bcd0a7ef936fd6b30a558d68
K V V S M SURYA NARAYANA MURTHY <k.madhavamurthy143@gmail.com>
2026-05-11 15:45:36 +0530

    Add username and email to update profile serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -61,6 +61,8 @@ class UpdateProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
+            'username',
+            'email',
             'phone',
             'bio',
             'linkedin_url',
